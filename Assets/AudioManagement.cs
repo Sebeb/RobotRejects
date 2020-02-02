3cd2609688c3cf036c4b58305addef01f895d800
Jonas Braukmann <jonas.braukmann@icloud.com>
2020-02-02 10:52:11 +0100

    Big Chungus of Change from yesterday
    Updated  and added sprites, AudioManager-Script added

---
+++ Assets/AudioManagement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManagement : MonoBehaviour
+{
+
+    public AudioSource master;
+    public AudioSource[] slaves;
+
+
+    private IEnumerator SyncSources()
+    {
+        while (true)
+        {
+            foreach (var slave in slaves)
+            {
+                slave.timeSamples = master.timeSamples;
+                yield return null;
+            }
+        }
+    }
+}
