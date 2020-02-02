fa69891c78b615381dc27e91044583b6fcc4cb23
Sebastian Parfitt <sebastian@shytea.games>
2020-02-02 13:12:46 +0100

    Improved garage
    Added uunpowered wheel

---
+++ Assets/Scripts/Objects/WheelObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelObject : BuildableObject
+{
+    public WheelJoint2D wheelJoint;
+    public float breakingForce = Mathf.Infinity;
+
+    public override void ConnectPivotToObject(BuildableObject _otherObject, PivotObject _pivot)
+    {
+        base.ConnectPivotToObject(_otherObject, _pivot);
+
+        wheelJoint = _otherObject.gameObject.AddComponent<WheelJoint2D>();
+        wheelJoint.connectedBody = rb;
+        wheelJoint.autoConfigureConnectedAnchor = false;
+        wheelJoint.anchor = _otherObject.transform.InverseTransformPoint(_pivot.transform.position);
+        wheelJoint.breakForce = breakingForce;
+    }
+
+    public override void DisconnectPivot(PivotObject _pivot)
+    {
+        base.DisconnectPivot(_pivot);
+
+        if (wheelJoint) { Destroy(wheelJoint); }
+    }
+}
